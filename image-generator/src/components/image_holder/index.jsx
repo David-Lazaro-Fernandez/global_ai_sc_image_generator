11beaff78e1d62b7ce04f32748034bd0f27d37d1
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2022-12-13 03:25:53 -0600

    Update index.jsx

--- image-generator/src/components/image_holder/index.jsx
+++ image-generator/src/components/image_holder/index.jsx
@@ -1,14 +1,14 @@
 import './Image_holder.css';
-const Image_holder = (props) =>{
+const ImageHolder = (props) =>{
  const {image} = props
  return(
     <>
         <div class="holder">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Picture_icon_BLACK.svg/1200px-Picture_icon_BLACK.svg.png"  />
+            <img alt="icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Picture_icon_BLACK.svg/1200px-Picture_icon_BLACK.svg.png"  />
             <p>Generated images will appear here!</p>
         </div>
     </>
  )
 }
 
-export default Image_holder;
+export default ImageHolder;
